c6dbe9f66e42c7bb7bb83147a334b012cc766318
hjkangIB <hjkang@idealbloom.io>
2024-06-21 13:00:57 +0900

    최종 상태값

--- src/xr-session.js
+++ src/xr-session.js
@@ -4,6 +4,19 @@ import { BufferGeometryUtils } from "three/examples/jsm/utils/BufferGeometryUtil
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { InstancedMesh, Vector3 } from "three";
 
+// 모바일 웹 주소창 숨기기
+window.addEventListener(
+  "load",
+  function () {
+    // body의 height를 살짝 늘리는 코드
+    document.body.style.height =
+      document.documentElement.clientHeight + 5 + "px";
+    // scroll를 제어 하는 코드
+    setTimeout(scrollTo, 0, 0, 1);
+  },
+  false
+);
+
 let axis;
 let isAxis = false;
 
